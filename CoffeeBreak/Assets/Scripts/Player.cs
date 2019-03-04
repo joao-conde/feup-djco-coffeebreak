1a6b6bfad1faa0c2d3005266f418782b91848392
Carlos Miguel da Silva de Freitas <cmiguel1997@gmail.com>
2019-03-04 19:11:21 +0000

    Change animator to use blend trees

--- CoffeeBreak/Assets/Scripts/Player.cs
+++ CoffeeBreak/Assets/Scripts/Player.cs
@@ -10,13 +10,18 @@ public class Player : MonoBehaviour{
     public float moveSpeed;
     private Animator playerAnimator;
 
+    private bool playerMoving;
+    private Vector2 lastMove;
+
     private void Start() {
         coins = GameManager.instance.initialPlayerCoins;
         playerAnimator = GetComponent<Animator>();
+
     }
 
     private void Update() {
 
+        playerMoving = false;
         if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f){
             if (Input.GetButton("SlowMovement"))
             {
@@ -28,7 +33,8 @@ public class Player : MonoBehaviour{
                 playerAnimator.speed = 1f;
                 transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f) * moveSpeed * Time.deltaTime);
             }
-                
+            playerMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
 
         if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f){
@@ -42,11 +48,15 @@ public class Player : MonoBehaviour{
                 playerAnimator.speed = 1f;
                 transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f) * moveSpeed * Time.deltaTime);
             }
-                
+            playerMoving = true;
+            lastMove = new Vector2(0f,Input.GetAxisRaw("Vertical"));
         }
 
         playerAnimator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         playerAnimator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        playerAnimator.SetBool("PlayerMoving", playerMoving);
+        playerAnimator.SetFloat("LastMoveX", lastMove.x);
+        playerAnimator.SetFloat("LastMoveY", lastMove.y);
     }
 
 }
