26446ffcf3d270aad30272eb77a111d4fad3abea
Carlos Miguel da Silva de Freitas <cmiguel1997@gmail.com>
2019-03-04 18:41:16 +0000

    Started project with simple gameManager, player movement and animation

---
+++ CoffeeBreak/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour{
+    
+    public float levelStartDelay = 2f;
+
+    public static GameManager instance = null;
+    public int initialPlayerCoins = 0;
+    private int level = 1;
+    private bool doingSetup;
+
+
+    private void Awake() {
+        if(instance == null)
+            instance = this;
+        else if(instance != this)
+            Destroy(gameObject);
+        
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    void InitGame(){
+
+    }
+
+    private void Update() {
+        
+    }
+
+    public void GameOver(){
+
+    }
+
+
+}
---
+++ CoffeeBreak/Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour{
+
+    private int coins;
+    public float moveSpeed;
+    private Animator playerAnimator;
+
+    private void Start() {
+        coins = GameManager.instance.initialPlayerCoins;
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f){
+            if (Input.GetButton("SlowMovement"))
+            {
+                playerAnimator.speed = 0.5f;
+                transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f) * moveSpeed / 2f * Time.deltaTime);
+            }
+            else
+            {
+                playerAnimator.speed = 1f;
+                transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f) * moveSpeed * Time.deltaTime);
+            }
+                
+        }
+
+        if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f){
+            if (Input.GetButton("SlowMovement"))
+            {
+                playerAnimator.speed = 0.5f;
+                transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f) * moveSpeed/2f * Time.deltaTime);
+            }
+            else
+            {
+                playerAnimator.speed = 1f;
+                transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f) * moveSpeed * Time.deltaTime);
+            }
+                
+        }
+
+        playerAnimator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        playerAnimator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+    }
+
+}
