8a080da26cc94019ff6f195196ee792a765ae602
Carlos Miguel da Silva de Freitas <cmiguel1997@gmail.com>
2019-03-04 19:34:13 +0000

    Made script so camera follows a specific target

---
+++ CoffeeBreak/Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject followTarget;
+    private Vector3 targetPosition;
+    public float moveSpeed;
+
+    private void Update()
+    {
+        targetPosition = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed*Time.deltaTime);
+    }
+}
