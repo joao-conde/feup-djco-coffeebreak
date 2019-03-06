3661f88a1250b341abae36c92d43303b58b9e3bf
Carlos Freitas <cmiguel1997@gmail.com>
2019-03-06 17:29:46 +0000

    Added coins to pick up

--- CoffeeBreak/Assets/Scripts/Player.cs
+++ CoffeeBreak/Assets/Scripts/Player.cs
@@ -8,28 +8,45 @@ public class Player : MonoBehaviour{
 
     private int coins;
     public float moveSpeed;
+
+    public float throwSpeed;
     private Animator playerAnimator;
 
     private bool playerMoving;
     private Vector2 lastMove;
 
+    private Rigidbody2D rb;
+
+   
+
     private void Start() {
         coins = GameManager.instance.initialPlayerCoins;
         playerAnimator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        coins = GameManager.instance.initialPlayerCoins;
 
+    }
+
+    private void FixedUpdate(){
+        
     }
 
     private void Update() {
 
+        PlayerMovement();
+        
+    }
+
+    
+
+    private void PlayerMovement(){
         playerMoving = false;
         if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f){
-            if (Input.GetButton("SlowMovement"))
-            {
+            if (Input.GetButton("SlowMovement")){
                 playerAnimator.speed = 0.5f;
                 transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f) * moveSpeed / 2f * Time.deltaTime);
             }
-            else
-            {
+            else{
                 playerAnimator.speed = 1f;
                 transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f) * moveSpeed * Time.deltaTime);
             }
@@ -38,13 +55,11 @@ public class Player : MonoBehaviour{
         }
 
         if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f){
-            if (Input.GetButton("SlowMovement"))
-            {
+            if (Input.GetButton("SlowMovement")){
                 playerAnimator.speed = 0.5f;
                 transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f) * moveSpeed/2f * Time.deltaTime);
             }
-            else
-            {
+            else{
                 playerAnimator.speed = 1f;
                 transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f) * moveSpeed * Time.deltaTime);
             }
@@ -57,6 +72,14 @@ public class Player : MonoBehaviour{
         playerAnimator.SetBool("PlayerMoving", playerMoving);
         playerAnimator.SetFloat("LastMoveX", lastMove.x);
         playerAnimator.SetFloat("LastMoveY", lastMove.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Coin")){
+            other.gameObject.SetActive(false);
+            coins++;
+            Debug.Log(coins);
+        }    
     }
 
 }
