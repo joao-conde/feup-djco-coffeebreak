dae5ff10c37d97179aa2839f79b1eb476b8d883f
Carlos Freitas <cmiguel1997@gmail.com>
2019-03-11 18:39:12 +0000

    Guard now detects player

--- CoffeeBreak/Assets/Scripts/GuardController.cs
+++ CoffeeBreak/Assets/Scripts/GuardController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GuardController : AIController{
+public class GuardController : AIController {
 
     public Light flashlight;
 
@@ -10,46 +10,57 @@ public class GuardController : AIController{
 
     private GameObject targetCoin = null;
     private float stopThreshold = 0.20f;
-    
+
     // Start is called before the first frame update
 
-    protected  void Start()    {
-        base.Start();
-        
+    protected void Start () {
+        base.Start ();
+
     }
 
     // Update is called once per frame
-    protected override void Update()
-    {
-       base.Update();
-       Vector3 direction = Vector3.Normalize(agent.velocity);
-       flashlight.transform.forward = Vector3.RotateTowards(flashlight.transform.forward,direction,rotationSpeed*Time.deltaTime,0.0f);
-       HandleDistraction();
+    protected override void Update () {
+        base.Update ();
+        Vector3 direction = Vector3.Normalize (agent.velocity);
+        flashlight.transform.forward = Vector3.RotateTowards (flashlight.transform.forward, direction, rotationSpeed * Time.deltaTime, 0.0f);
+        HandleDistraction ();
     }
 
+    protected void FixedUpdate () {
+        RaycastHit2D[] hit = Physics2D.RaycastAll((Vector2)gameObject.transform.position+(Vector2)flashlight.transform.forward.normalized,((Vector2)flashlight.transform.forward.normalized), 4.5f);
+        Debug.DrawLine ((Vector2)gameObject.transform.position + (Vector2)flashlight.transform.forward.normalized, (Vector2)gameObject.transform.position + (Vector2)flashlight.transform.forward.normalized * 4.5f);
 
-    //TODO: avoid catching coins not thrown
-    private void HandleDistraction(){
-        if(targetCoin != null){
-            Rigidbody2D rbCoin = targetCoin.GetComponent<Rigidbody2D>();
-            
-            //stopped coin
-            if(rbCoin.velocity.x <= stopThreshold && rbCoin.velocity.y <= stopThreshold){ 
-                agent.SetDestination(rbCoin.position);
+        if(hit.Length > 1){
+            Debug.Log(hit[1].collider.tag);
+            if(hit[1].collider.CompareTag("Player")){
+                Debug.Log("Saw player");
             }
-            
+        }
+        
+        
+        
+    }
+
+    //TODO: avoid catching coins not thrownlayerMask
+    private void HandleDistraction () {
+        if (targetCoin != null) {
+            Rigidbody2D rbCoin = targetCoin.GetComponent<Rigidbody2D> ();
+
+            //stopped coin
+            if (rbCoin.velocity.x <= stopThreshold && rbCoin.velocity.y <= stopThreshold) {
+                agent.SetDestination (rbCoin.position);
+            }
+
             //on coin
-            if(agent.remainingDistance < 1f){
-                Destroy(targetCoin);
+            if (agent.remainingDistance < 1f) {
+                Destroy (targetCoin);
                 targetCoin = null;
-            } 
+            }
         }
     }
-    
-    
 
-    private void OnTriggerStay2D(Collider2D collision) {
-        if(collision.gameObject.CompareTag("Coin") && targetCoin == null){
+    private void OnTriggerStay2D (Collider2D collision) {
+        if (collision.gameObject.CompareTag ("Coin") && targetCoin == null) {
             targetCoin = collision.gameObject;
         }
     }
