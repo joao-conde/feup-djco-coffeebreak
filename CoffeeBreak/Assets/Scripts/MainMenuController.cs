bbd2fcbc97b8b6837abedb67acad7af0f5ea4935
joao-conde <up201503256@fe.up.pt>
2019-03-15 14:36:37 +0000

    Main menu

---
+++ CoffeeBreak/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    public void Play () {
+        SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+    }
+
+    public void Quit () {
+        Debug.Log ("Quit only works in a compiled game, not in editor, but trust!");
+        Application.Quit ();
+    }
+
+}
