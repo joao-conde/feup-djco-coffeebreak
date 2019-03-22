e9f8caf76428c9323921d7996b5d8bc63708fc8e
joao-conde <up201503256@fe.up.pt>
2019-03-22 20:04:11 +0000

    Char by char text scene

---
+++ CoffeeBreak/Assets/Scripts/TextScroller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScroller : MonoBehaviour {
+
+    public float timeBetweenChars;
+
+    private string text;    
+    private Text display;
+
+    private IEnumerator coroutine;
+
+
+    void Start () {
+        display = gameObject.GetComponent<Text> (); 
+        text = display.text;
+        display.text = "";
+        coroutine = DisplayCharByChar ();
+        StartCoroutine(coroutine);  
+    }
+
+    void Update(){
+        if(Input.GetButtonDown ("Interact")){
+            StopCoroutine(coroutine);
+            display.text = text;
+        }
+    }
+
+    public IEnumerator DisplayCharByChar () {
+        foreach (char c in text) {
+            display.text += c;
+            yield return new WaitForSeconds (timeBetweenChars);
+        }
+    }
+
+}
